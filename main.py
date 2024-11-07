f22dfdfa486fb15c98a17fae0fc2b973662c5334
Doston <dostoniskandarov0204@gmail.com>
2024-11-07 21:54:45 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from collections import namedtuple
+
+# phones = (
+#     {"id": 1, "name": "Samsung Galaxy 1", "color": "red"},
+#     {"id": 2, "name": "Samsung Galaxy 2", "color": "blue"},
+#     {"id": 3, "name": "Samsung Galaxy 3", "color": "green"},
+#     {"id": 4, "name": "Samsung Galaxy 4", "color": "white"},
+#     {"id": 5, "name": "Samsung Galaxy 5", "color": "yellow"},
+# )
+#
+# Phone = namedtuple("Phone", "id name color")
+#
+# for i in phones:
+#     p = Phone(**i)
+#     print(p.id, p.name, p.color)
+
+# Vazifa________________________________________________________
+
+# 1
+
+# talabalar = (
+#     {"id": 1, "name": "Sardor", "surname": "Toxirov"},
+#     {"id": 2, "name": "Axror", "surname": "Fozilov"},
+#     {"id": 3, "name": "Abror", "surname": "Ibragimov"},
+# )
+#
+# Talaba = namedtuple("Talabalar", "id name surname")
+#
+# for i in talabalar:
+#     t = Talaba(**i)
+#     print(t.id, t.name, t.surname)
+
+# 2
+
+# maxsulotlar = (
+#     {"product_name": "apple", "price": 3000, "quantity": "1 t"},
+#     {"product_name": "granate", "price": 5000, "quantity": "750 kg"},
+#     {"product_name": "peach", "price": 8000, "quantity": "600 kg"},
+# )
+#
+# Maxsulot = namedtuple("Maxsulotlar", "product_name price quantity")
+#
+# for i in maxsulotlar:
+#     m = Maxsulot(**i)
+#     print(m.product_name, m.price, m.quantity)
+
+# 3
+
+# cities = (
+#     {"city_name": "Rim", "country": "Italy", "population": "3.000.000"},
+#     {"city_name": "Parij", "country": "Franch", "population": "5.000.000"},
+#     {"city_name": "London", "country": "UK", "population": "7.000.000"},
+# )
+#
+# Shahar = namedtuple("Shahar", "city_name country population")
+#
+# for i in cities:
+#     c = Shahar(**i)
+#     print(c.city_name, c.country, c.population)
+
+# 4
+#
+# cars = (
+#     {"make": "Alumin", "model": "Lambo", "year": 2020},
+#     {"make": "Alumin", "model": "BMW", "year": 2021},
+#     {"make": "Alumin", "model": "Mers", "year": 2022},
+# )
+#
+# Avtomobil = namedtuple("Avtomobil", "make model year")
+#
+# for i in cars:
+#     c = Avtomobil(**i)
+#     print(c.model, c.make, c.year)
+
